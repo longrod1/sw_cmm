e769852c18a39d74a2f3508971554c5b3e34ce09
rizc <rizc@k4tp3r>
2015-05-03 20:21:46 -0500

    agregacion de main.js
    archivos agregados

---
+++ cargar.js
@@ -0,0 +1,44 @@
+function getHttpObject(){
+  var XMLHttp;
+  if (window.XMLHttpRequest) {
+    XMLHttp = new XMLHttpRequest();
+  } else if (window.ActiveXObject) {
+    XMLHttp = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  return XMLHttp;
+}
+
+var http = getHttpObject();
+
+function Load(){    
+  var modelo = document.getElementById('selectModelo').value;
+  var mostrar = document.getElementById('datos');
+  if(http){
+    http.onreadystatechange = function(){
+      if(http.readyState==4 && http.status==200){
+        mostrar.innerHTML=http.responseText;
+      }
+    }
+    http.open("GET","cgi-bin/conection.pl?nombre="+nombre+"&codigo="+codigo+"&estado="+estado,true);
+    http.send();
+  }
+}
+
+
+function LoadDel(){
+  var ver = document.getElementById('datos');
+  var codigo = document.getElementById('codigo').value;
+  if(http){
+    http.onreadystatechange = function(){
+      if(http.readyState==4 && http.status==200){
+        ver.innerHTML=http.responseText;
+      }
+    }
+    http.open("GET","cgi-bin/eliminar.pl?codigo="+codigo,true);
+    http.send();
+  }
+}
+
+function alerta(){
+	alert("hola");
+}
